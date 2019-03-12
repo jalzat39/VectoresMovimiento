0f279c2d0550c3225a916e4a6d95ce063860cf39
pablobernalmoreno <pbernal2@eafit.edu.co>
2019-03-12 12:50:33 -0700

    pi

--- openmpvm.c
+++ openmpvm.c
@@ -3,11 +3,15 @@
 #include <time.h>
 #include <stdlib.h>
 
+
+
 int main(int argc, char const *argv[])
 {
-	int i, j, x, y;
-    int dx = 0;
-    int dy = 0:
+	int i, j;
+	int x = 0;
+	int y = 0;
+    int dx = 1;
+    int dy = 1;
     int val = 10000;
 	srand(time(NULL));
 	//int n = atoi(argv[1]);
@@ -17,8 +21,9 @@ int main(int argc, char const *argv[])
 	{
 		for(j = 0; j < n; j++)
 		{
-			vector__1[i][j] = (rand()%4)+1;
-			vector__2[i][j] = (rand()%4)+1;
+			//(rand()%4)+
+			vector__1[i][j] = 1;
+			vector__2[i][j] = 1;
 		}
 	}
 	#pragma omp parallel
@@ -27,16 +32,18 @@ int main(int argc, char const *argv[])
 		int i, j, k, suma = 0;
 		tid = omp_get_thread_num();
 		#pragma omp for 
-		for(i = 0; i < n; i++)
+		for(i = 0; i <= n ; i++)
 		{
-			for(j = 0; j < n; j++)
+			for(j = 0; j <= n ; j++)
 			{
 			    suma = (vector__1[x + i][y + y] - vector__2[x + i + dx][y + j + dy]);
-				if(val < suma){
+				if(val > suma){
                     val = suma;
                 }
 			}
 		}
+		printf("%d", tid);
+		printf("%d", val);
 	}
     // Print val pero no se como Xd
 	return 0;
