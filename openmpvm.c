504fc5ccb5e2bb674f30fb429f2384c4b564ba8a
dantivar <dantivar@eafit.edu.co>
2019-03-23 16:22:07 -0500

    Update openmpvm.c

--- openmpvm.c
+++ openmpvm.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <omp.h>
 #include <time.h>
 #include <stdlib.h>
@@ -13,27 +12,33 @@ int main(int argc, char const *argv[]){
   int dx = 1;
   int dy = 1;
   int val = 10000;
-  const char* imageName = argv[1];
-  Mat image;
-  image = imread(imageName, 1);
-  if(argc != 2 || !image.data){
+  const char* imageName1 = argv[1];
+  const char* imageName2 = argv[2];
+  Mat image1;
+  Mat image2;
+  image1 = imread(imageName1, 1);
+  image2 = imread(imageName2, 1);
+  if(argc != 3 || !image1.data || !image2.data){
     printf("No image data \n");
     return -1;
   }
-  Mat gray_image;
-  cvtColor(image, gray_image, COLOR_BGR2GRAY);
-  uint8_t *myData = gray_image.data;
-  int width = gray_image.cols;
-  int height = gray_image.rows;
-  int _stride = image.step;
+  Mat gray_image1;
+  Mat gray_image2;
+  cvtColor(image1, gray_image1, COLOR_BGR2GRAY);
+  cvtColor(image2, gray_image2, COLOR_BGR2GRAY);
+  uint8_t *myData1 = gray_image1.data;
+  uint8_t *myData2 = gray_image2.data;
+  int width = gray_image1.cols;
+  int height = gray_image1.rows;
+  int _stride = image1.step;
   srand(time(NULL));
-  //int n = atoi(argv[1]);
   int vector__1[height][width], vector__2[height][width];
   for(i = 0; i < height; i++){
     for(j = 0; j < width; j++){
-      uint8_t val = myData[i * _stride + j];
-      vector__1[i][j] = val;
-      vector__2[i][j] = val;
+      uint8_t val1 = myData1[i * _stride + j];
+      uint8_t val2 = myData2[i * _stride + j];
+      vector__1[i][j] = val1;
+      vector__2[i][j] = val2;
     }
   }
   #pragma omp parallel
